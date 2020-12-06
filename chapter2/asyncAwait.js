a5164ee131c1ee815a6da037c1d60bb6360446aa
kazuhi-ra <kazuhira4126@gmail.com>
2020-12-06 18:21:08 +0900

    github APIからusrInfoを取り出せるように

--- chapter2/asyncAwait.js
+++ chapter2/asyncAwait.js
@@ -1,14 +1,16 @@
 const fetch = require('node-fetch')
 
-const userIds = ['kazuhi-ra', 'hitode909', 'pasta-k']
+const userIds = ['kazuhi-ra', 'hitode909', 'pastak']
 
 const yo = async () => {
   try {
     const response = await fetch(
       `https://api.github.com/users/${encodeURIComponent(userIds[2])}`
     )
+    const userInfo = await response.json()
 
-    console.log(response)
+    // console.log(response)
+    console.log(userInfo)
   } catch (e) {
     console.log(e)
   }
