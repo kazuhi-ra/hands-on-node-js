46784cb76b8b7f6c7e5312946dfab30cdbd0e048
kazuhi-ra <kazuhira4126@gmail.com>
2020-12-09 14:19:51 +0900

    自前のwritableStreamを実装してみる

---
+++ chapter3/writableStream.js
@@ -0,0 +1,19 @@
+const stream = require('stream')
+
+class DelayLogStream extends stream.Writable {
+  constructor(options) {
+    super({ objectMode: true, ...options })
+  }
+
+  _write(chunk, encoding, callback) {
+    console.log('_write()')
+    const { message, delay } = chunk
+    setTimeout(() => {
+      console.log(message)
+      callback()
+    }, delay)
+  }
+}
+
+const delayLogStream = new DelayLogStream()
+delayLogStream.write({ message: 'Yo', delay: 0 })
